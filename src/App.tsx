788efd2577c0b9cdb1d10285ae26b426c6848ce7
YamaguchiRyuta <steven_siro_vai@hotmail.co.jp>
2021-05-14 16:43:56 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,61 @@
 import React, { useState } from 'react';
 import { MyButton } from './components/MyButton';
 import { PassPropState } from './components/PassPropState';
+import { PassPropState2 } from './components/PassPropState2';
 import { Graph } from './components/Graph';
+import { BarGraph } from './components/BarGraph';
+import { Divider } from '@material-ui/core';
+import { DatePicker } from './components/DatePicker';
+
+const graphProp = {
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: 'Dataset',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        axis: 'y',
+        beginAtZero: true,
+        reverse: true,
+      },
+    },
+  },
+};
 
 const App: React.VFC = () => {
-  const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState<string>('default');
 
   return (
     <>
+      <BarGraph {...graphProp} />
+      <Divider />
+      <DatePicker />
+      <Divider />
+      <Graph />
+      <Divider />
       <MyButton
         variant="contained"
         color="primary"
@@ -16,9 +64,10 @@ const App: React.VFC = () => {
           console.log('Primary Button');
         }}
       />
+      <Divider />
       <PassPropState defaultValue={''} />
-
-      <Graph />
+      <Divider />
+      <PassPropState2 state={[value, setValue]} />
     </>
   );
 };
